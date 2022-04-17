2bf314c1bae007271ae98d889ea8ad4327d476be
nopeless <nopeless@users.noreply.github.com>
2022-04-17 22:19:43 +0900

    edits

--- src/mem-cache.ts
+++ src/mem-cache.ts
@@ -1,12 +1,31 @@
+import { PriorityQueue } from "./priority-queue.js";
+
 class MemoryCache<T> {
   public ttl: number;
-  constructor({ ttl = -1 }: { ttl: number }) {
+  private pq = new PriorityQueue<TimedItem<T>>();
+  public cache = new Map<string, T>();
+  constructor({ ttl = -1 }: { ttl?: number } = {}) {
     this.ttl = ttl;
   }
+
+  // get(idx: string): T | null {
+  //   const item = this.cache.get(idx);
+  //   if (item) {
+  //     this.pq.removeOne((v) => v.value === item);
+  //     return item;
+  //   }
+  //   return null;
+  // }
+
+  add(key: string, value: T) {}
+}
+
+class TimedItem<T> {
+  constructor(public ttl: number, public value: T) {}
 }
 
 function MemoryCacheProxy<T>() {
   return new Proxy(Object.create(null), {});
 }
 
-export {};
+export { MemoryCache, MemoryCacheProxy };
